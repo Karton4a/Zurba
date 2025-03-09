f0e05ddf1c0f69a8d229d4d2e7bf9fc1ac084cec
Orange_Karton <anpaschenko@gmail.com>
2025-03-09 17:11:46 +0200

    windows style path

--- zurba.lua
+++ zurba.lua
@@ -15,7 +15,7 @@ project(PROJECT_NAME)
    targetdir "bin/%{cfg.buildcfg}"
    cppdialect "C++20"
 
-   postbuildcommands { 'rd /s /q "bin/%{cfg.buildcfg}/data" & xcopy data "bin/%{cfg.buildcfg}/data" /s /i' }
+   postbuildcommands { 'rd /s /q bin\\%{cfg.buildcfg}\\data & xcopy data bin\\%{cfg.buildcfg}\\data /s /i' }
 
    includedirs { "external" }
 
